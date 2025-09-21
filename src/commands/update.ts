8b9c288db43ec57603e1cc457927695bd380c1ff
T-Kisaku <kisakutakashi0226@gmail.com>
2025-09-21 16:36:00 +0900

    up

--- src/commands/update.ts
+++ src/commands/update.ts
@@ -4,7 +4,7 @@ export default new Command()
   .description(`Update ${binaryName} cli`)
   .action(async () => {
     const url =
-      "https://raw.githubusercontent.com/T-Kisaku/nikki-cli/main/install.sh";
+      "https://raw.githubusercontent.com/T-Kisaku/nikki-cli/main/scripts/install.sh";
     const cmd = new Deno.Command("sh", {
       args: ["-c", `curl -fsSL ${url} | sh`],
       stdout: "inherit",
